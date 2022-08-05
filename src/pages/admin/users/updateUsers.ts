788bebc93727d340ba835bc58f55cba7cd59ac31
tuananh-lc <anhdhtph16661@fpt.edu.vn>
2022-08-05 12:03:24 +0700

    #41 - update create update UserPage

--- src/pages/admin/users/updateUsers.ts
+++ src/pages/admin/users/updateUsers.ts
@@ -1,33 +1,51 @@
+import { UpdateNewUser, userGetOne } from "../../../api/users";
 import HeaderAdmin from "../../../components/Header/Admin"
 import Sidebar from "../../../components/Sidebar/slibarAdmin"
+import { IUsers } from "../../../Interface/IUsers";
 
 const UpdateUser = {
     async render(id: any) {
 
+        const UserData = await userGetOne(id);
+        const User: IUsers = UserData.data.user
+        console.log(User);
         
-        return `
+        return /*html*/`
             ${HeaderAdmin.render()}
             <div class="flex justify-between px-[30px] mt-[100px]">
             ${Sidebar.render()}
             <div class="card-body h-[600px] overflow-y-scroll w-full bg-[#E5E5E5]">
             <div class="ml-[20px] w-full p-[25px] max-h-[100%] h-full">
                 <div class="">
-                    <h2 class="capitalize text-[20px] leading-[30px] text-[#5F5E61] font-bold">cập nhật danh mục</h2>
+                    <h2 class="capitalize text-[20px] leading-[30px] text-[#5F5E61] font-bold">cập nhật người dùng</h2>
                     
                     <div class="flex justify-between items-start mb-[50px]">
                         <div class="max-w-[600px] w-full pt-[30px]" >
 
                             <div>
                                 <div class="mt-[10px]">
-                                    <label for="name" class="capitalize text-[16px] leading-[19px] text-[#5A6169]">tên danh mục</label><br>
-                                    <input type="type" id="name" class="check-validate w-full mt-[5px] h-[35px] rounded-sm outline-0 pl-[10px] capitalize" value="${category.category.name}">
+                                    <label for="name" class="capitalize text-[16px] leading-[19px] text-[#5A6169]">tên người dùng</label><br>
+                                    <input type="text" id="name" class="check-validate w-full mt-[5px] h-[35px] rounded-sm outline-0 pl-[10px] capitalize" value="${User.name}">
                                     <span class="error-input block text-red-500 text-xs "><span>
                                 </div>
+
                                 <div class="mt-[10px]">
-                                    <label for="name" class="capitalize text-[16px] leading-[19px] text-[#5A6169]">icon</label><br>
-                                    <input type="type" id="icon" class="check-validate w-full mt-[5px] h-[35px] rounded-sm outline-0 pl-[10px] capitalize" value="${category.category.icon}">
+                                    <label for="name" class="capitalize text-[16px] leading-[19px] text-[#5A6169]">gmail</label><br>
+                                    <input type="text" id="email" class="check-validate w-full mt-[5px] h-[35px] rounded-sm outline-0 pl-[10px]" value="${User.email}">
                                     <span class="error-input block text-red-500 text-xs "><span>
                                 </div>
+
+                                <div class="mt-[10px]">
+                                <label for="name" class="capitalize text-[16px] leading-[19px] text-[#5A6169]">password</label><br>
+                                <input type="text" id="password" class="check-validate w-full mt-[5px] h-[35px] rounded-sm outline-0 pl-[10px]" value="${User.password}">
+                                <span class="error-input block text-red-500 text-xs "><span>
+                            </div>
+
+                            <div class="mt-[10px]">
+                            <label for="name" class="capitalize text-[16px] leading-[19px] text-[#5A6169]">role</label><br>
+                            <input type="number" id="role" class="check-validate w-full mt-[5px] h-[35px] rounded-sm outline-0 pl-[10px] " value="${User.role}">
+                            <span class="error-input block text-red-500 text-xs "><span>
+                        </div>
                                 <div class="mt-[20px]">
                                     <button type="submit" class="capitalize bg-[#00B0D7] text-[#fff] rounded-lg px-[20px] py-[10px]" id="btnUpdate">cập nhật</button>
                                 </div>
@@ -48,16 +66,18 @@ const UpdateUser = {
         console.log(btnUpdate);
         btnUpdate?.addEventListener("click", async function (e: any) {
             e.preventDefault();
-            const category = {
+            const users = {
                 _id: id,
                 name: document.getElementById('name')?.value,
-                icon: document.getElementById('icon')?.value
+                email: document.getElementById('email')?.value,
+                password: document.getElementById('password')?.value,
+                role: Number(document.getElementById('role')?.value)
             };
-            console.log(category);
-            const result = await UpdateCategory(category, id);
+            console.log(users);
+            const result = await UpdateNewUser(users, id);
             if(result){
                 alert("Cập nhật thành công");
-                location.href = "/admin/category"
+                location.href = "/admin/users"
             }
 
 
