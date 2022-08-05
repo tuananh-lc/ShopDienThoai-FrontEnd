a8ea4a953763e1573ba4cf467a334e96b268881f
datdevelop2802 <79825535+datdepzai2802@users.noreply.github.com>
2022-08-05 12:52:15 +0700

    Update cartProducts.ts

--- src/pages/cartProducts.ts
+++ src/pages/cartProducts.ts
@@ -5,8 +5,7 @@ import { $$ } from "./utilities/utiliti";
 const cartProducts = {
     render() {
         const newCart = JSON.parse(localStorage.getItem("cart"))
-        console.log(newCart, "localstorage");
-            
+            console.log(newCart, "localstorage");
         return /*html*/`
             ${HeaderUser.render()}
             
@@ -25,35 +24,17 @@ const cartProducts = {
                             >
                         </div>
                         </div>
-                        
-                        ${newCart?.map((newCart: any) => /*html*/`
-                        <div
-                        class="mt-[30px] shadow-xl py-[20px] px-[20px] rounded-xl flex justify-between items-start"
-                        >
-                        <div>
-                            <img
-                            src="${newCart.image}"
-                            alt=""
-                            class="w-full max-w-[180px]"
-                            />
-                        </div>
-                        <div class="ml-[10px]">
-                            <h3 class="capitalize text-[18px] text-[#333] font-medium">
-                            ${newCart.name}
-                            </h3>
-                            <div class="flex items-center justify-between">
-                            <span class="text-red-500 text-[16px] font-medium"
-                                >${newCart.sale?.toLocaleString('vi', {style : 'currency', currency : 'VND'})}</span
+                        ${
+                                newCart?newCart.map((newCart: any) => /*html*/`
+                            <div
+                            class="mt-[30px] shadow-xl py-[20px] px-[20px] rounded-xl flex justify-between items-start"
                             >
-                            <span id="prices" class="text-[#888] line-through text-[14px]"
-                                >${newCart.price?.toLocaleString('vi', {style : 'currency', currency : 'VND'})}</span
-                            >
-                            <span
-                                class="py-[5px] px-[10px] bg-red-600 text-white capitalize ml-[10px] rounded-lg inline-block"
-                            >
-                                giảm ${((newCart.price-newCart.sale)/newCart.price *100).toFixed(0)}%
-                            </span>
-
+                            <div>
+                                <img
+                                src="${newCart.image}"
+                                alt=""
+                                class="w-full max-w-[180px]"
+                                />
                             </div>
                             <div class="ml-[10px]">
                                 <h3 class="capitalize text-[18px] text-[#333] font-medium">
@@ -73,29 +54,28 @@ const cartProducts = {
                                 </span>
                                 </div>
 
-
-                            <div class="flex justify-between mt-[10px] w-full max-w-[180px]">Số lượng
+                                <div class="flex justify-between mt-[10px] w-full max-w-[180px]">
+                                    <span class="text-[20px] border-[1px] border-[#505050] w-full border-r-[0px] text-center cursor-pointer outline-none" id="cong"> + </span>
                                         <input type="number" class="outline-none text-[14px] text-[#333] max-w-[90px] text-center pl-[25px]" id="amount" disabled value="${newCart.amount}">
-                                    
-                            <!--  -->
+                                    <span class="text-[20px] border-[1px] border-[#505050] w-full border-l-[0px] text-center cursor-pointer outline-none" id="tru"> - </span>
+                                    <!--  -->
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <button>
-                            <i class="fa-solid fa-xmark text-[20px] text-[#777]"></i>
-                            </button>
-                        </div>
-                        </div>
-                        
-
+                            <div>
+                                <button>
+                                <i class="fa-solid fa-xmark text-[20px] text-[#777]"></i>
+                                </button>
+                            </div>
+                            </div>
+                            `).join(""):"Chưa có sản phẩm nào"
+                        } 
                         <div class="mt-[50px] shadow-lg p-[20px]">
                         <div class="flex justify-between">
                             <h3 class="text-[18px] capitalize font-medium">
                             tổng tiền tạm tính:
                             </h3>
-                            <span class="text-red-500 text-[16px] font-bold">${(newCart.amount * newCart.price)?.toLocaleString('vi', {style : 'currency', currency : 'VND'})}</span>
+                            <span class="text-red-500 text-[16px] font-bold">17.820.000 ₫</span>
                         </div>
-                        `).join("")}
                         <button
                             class="block bg-red-500 w-full mt-[10px] py-[15px] text-white capitalize font-bold transition delay-150 duration-300 ease-in-out hover:bg-red-600 rounded-lg"
                         >
@@ -128,7 +108,6 @@ const cartProducts = {
             })
         })
     }
-
 }
 
 export default cartProducts
